24d1973b7a292ad7c813abfcd7300888ac359fdd
lucasfrezzini <luquiitas.f@gmail.com>
2021-11-23 16:43:47 -0300

    Mejoras css en skeletons

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Button.scss";
 
-const Button = ({text, icon, type, variant, cb}) => {
+const Button = ({text, icon, type, variant}) => {
 	return (
 		<button type={type} className={`Button ${variant}`}>{text} {icon}</button>
 	)
